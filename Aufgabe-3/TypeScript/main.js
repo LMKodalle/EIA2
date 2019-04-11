01cfea19ce7711a52034148055fc44eaac5ab1c6
Lukas Kodalle <LMKodalle@gmail.com>
2019-04-11 15:15:01 +0200

    update

--- Aufgabe-3/TypeScript/main.js
+++ Aufgabe-3/TypeScript/main.js
@@ -1,143 +1,178 @@
 let herz7 = {
     farbe: "Herz",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 1
 };
 let herz8 = {
     farbe: "Herz",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 2
 };
 let herz9 = {
     farbe: "Herz",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 3
 };
 let herz10 = {
     farbe: "Herz",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 4
 };
 let herzAss = {
     farbe: "Herz",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 5
 };
 let herzBube = {
     farbe: "Herz",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 6
 };
 let herzDame = {
     farbe: "Herz",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 7
 };
 let herzKönig = {
     farbe: "Herz",
-    zahl: "König"
+    zahl: "König",
+    order: 8
 };
 let karo7 = {
     farbe: "Karo",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 9
 };
 let karo8 = {
     farbe: "Karo",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 10
 };
 let karo9 = {
     farbe: "Karo",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 11
 };
 let karo10 = {
     farbe: "Karo",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 12
 };
 let karoAss = {
     farbe: "Karo",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 13
 };
 let karoBube = {
     farbe: "Karo",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 14
 };
 let karoDame = {
     farbe: "Karo",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 15
 };
 let karoKönig = {
     farbe: "Karo",
-    zahl: "König"
+    zahl: "König",
+    order: 16
 };
 let pik7 = {
     farbe: "Pik",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 17
 };
 let pik8 = {
     farbe: "Pik",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 18
 };
 let pik9 = {
     farbe: "Pik",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 19
 };
 let pik10 = {
     farbe: "Pik",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 20
 };
 let pikAss = {
     farbe: "Pik",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 21
 };
 let pikBube = {
     farbe: "Pik",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 22
 };
 let pikDame = {
     farbe: "Pik",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 23
 };
 let pikKönig = {
     farbe: "Pik",
-    zahl: "König"
+    zahl: "König",
+    order: 23
 };
 let kreuz7 = {
     farbe: "Kreuz",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 24
 };
 let kreuz8 = {
     farbe: "Kreuz",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 25
 };
 let kreuz9 = {
     farbe: "Kreuz",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 26
 };
 let kreuz10 = {
     farbe: "Kreuz",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 27
 };
 let kreuzAss = {
     farbe: "Kreuz",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 28
 };
 let kreuzBube = {
     farbe: "Kreuz",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 29
 };
 let kreuzDame = {
     farbe: "Kreuz",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 30
 };
 let kreuzKönig = {
     farbe: "Kreuz",
-    zahl: "König"
+    zahl: "König",
+    order: 31
 };
-document.addEventListener('DOMContentLoaded', write_htmlbody);
+document.addEventListener("DOMContentLoaded", write_htmlbody);
 function write_htmlbody() {
     let body = `<div class="top_interface">
     <div>
         <p class="rundenzahl">Rundenzahl:</p>
     </div>
         <p class="countdown">30s/20s/10s</p>`;
-    document.getElementById("createHtml").insertAdjacentHTML('beforeend', body);
+    document.getElementById("createHtml").insertAdjacentHTML("beforeend", body);
     neues_spiel();
+    document.getElementById("ziehstapel").addEventListener("click", cards_ziehen);
+    document.getElementById("sort").addEventListener("click", cards_sort);
 }
 let ziehstapel = [herz7, herz8, herz9, herz10, herzAss, herzBube, herzDame, herzKönig, karo7, karo8, karo9, karo10, karoAss, karoBube, karoDame, karoKönig, pik7, pik8, pik9, pik10, pikAss, pikBube, pikDame, pikKönig, kreuz7, kreuz8, kreuz9, kreuz10, kreuzAss, kreuzBube, kreuzDame, kreuzKönig];
 let handkarten = [];
+let ablagestapel = [];
 function neues_spiel() {
     let anfangskarten;
     ziehstapel = [herz7, herz8, herz9, herz10, herzAss, herzBube, herzDame, herzKönig, karo7, karo8, karo9, karo10, karoAss, karoBube, karoDame, karoKönig, pik7, pik8, pik9, pik10, pikAss, pikBube, pikDame, pikKönig, kreuz7, kreuz8, kreuz9, kreuz10, kreuzAss, kreuzBube, kreuzDame, kreuzKönig];
@@ -146,10 +181,10 @@ function neues_spiel() {
     do {
         anfangskarten = parseInt(prompt("Wie viele Handkarten? Von einer bis 32"), 10);
     } while (isNaN(anfangskarten) || anfangskarten > 32 || anfangskarten < 1);
-    ;
     document.getElementById("handkartenAnzahl").innerHTML = "Anzahl Handkarten:" + " " + anfangskarten;
     random_cards(anfangskarten);
     console.log(anfangskarten);
+    document.getElementById("neuesspiel").addEventListener("click", neues_spiel);
 }
 function random_cards(_anfangskarten) {
     let i = 0;
@@ -164,6 +199,7 @@ function random_cards(_anfangskarten) {
         ziehstapel.splice(x, 1);
         let kartenFarbe = handkarten[i].farbe;
         let kartenNummer = handkarten[i].zahl;
+        let kartenID = handkarten[i].order;
         console.log(kartenFarbe, kartenNummer);
         create_cards(kartenFarbe, kartenNummer);
         i++;
@@ -175,31 +211,41 @@ function create_cards(_kartenFarbe, _kartenNummer) {
         case "Herz":
         case "Karo":
             let cardsred = `<div class="flex"><ul><li class="farberot">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
-            document.getElementById("handkarten").insertAdjacentHTML('beforeend', cardsred);
+            document.getElementById("handkarten").insertAdjacentHTML("beforeend", cardsred);
             break;
         default:
             let cardsblack = `<div class="flex"><ul><li class="farbeschwarz">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
-            document.getElementById("handkarten").insertAdjacentHTML('beforeend', cardsblack);
+            document.getElementById("handkarten").insertAdjacentHTML("beforeend", cardsblack);
             break;
     }
 }
 function create_ziehstapel(_ziehstapelanzahl) {
-    let ziehstapel = `<div class="ziehstapel"><p id="innerZiehstapel"></p></div>`;
-    document.getElementById("handkarten").insertAdjacentHTML('beforeend', ziehstapel);
+    let ziehstapel = `<div class="ziehstapel" id="ziehstapel"><p id="innerZiehstapel"></p></div>`;
+    document.getElementById("ziehstapel").insertAdjacentHTML("beforeend", ziehstapel);
     document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + _ziehstapelanzahl;
 }
 function cards_ziehen() {
-    let i = 0;
     let x;
-    while (i < 1) {
-        x = Math.floor((Math.random() * ziehstapel.length) + 0);
-        handkarten.push(ziehstapel[x]);
-        ziehstapel.splice(x, 1);
-        let kartenFarbe = handkarten[handkarten.length - 1].farbe;
-        let kartenNummer = handkarten[handkarten.length - 1].zahl;
-        create_cards(kartenFarbe, kartenNummer);
-        document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + ziehstapel.length;
+    x = Math.floor((Math.random() * ziehstapel.length) + 0);
+    handkarten.push(ziehstapel[x]);
+    ziehstapel.splice(x, 1);
+    let kartenFarbe = handkarten[handkarten.length - 1].farbe;
+    let kartenNummer = handkarten[handkarten.length - 1].zahl;
+    create_cards(kartenFarbe, kartenNummer);
+    document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + ziehstapel.length;
+    document.getElementById("handkartenAnzahl").innerHTML = "Anzahl Handkarten:" + " " + handkarten.length;
+}
+function cards_sort() {
+    let i = 0;
+    document.getElementById("handkarten").innerHTML = " ";
+    handkarten.sort(array_sort);
+    console.log(handkarten);
+    while (i < handkarten.length) {
+        create_cards(handkarten[i].farbe, handkarten[i].zahl);
         i++;
     }
+}
+function array_sort(_a, _b) {
+    return _a.order - _b.order;
 }
 //# sourceMappingURL=main.js.map
--- Aufgabe-3/TypeScript/main.ts
+++ Aufgabe-3/TypeScript/main.ts
@@ -1,223 +1,272 @@
 interface Spielkarte {
     farbe: string;
     zahl: string;
+    order: number;
 }
 let herz7: Spielkarte = {
     farbe: "Herz",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 1
 };
 let herz8: Spielkarte = {
     farbe: "Herz",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 2
 };
 let herz9: Spielkarte = {
     farbe: "Herz",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 3
 };
 let herz10: Spielkarte = {
     farbe: "Herz",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 4
 };
 let herzAss: Spielkarte = {
     farbe: "Herz",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 5
 };
 let herzBube: Spielkarte = {
     farbe: "Herz",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 6
 };
 let herzDame: Spielkarte = {
     farbe: "Herz",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 7
 };
 let herzKönig: Spielkarte = {
     farbe: "Herz",
-    zahl: "König"
+    zahl: "König",
+    order: 8
 };
 let karo7: Spielkarte = {
     farbe: "Karo",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 9
 };
 let karo8: Spielkarte = {
     farbe: "Karo",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 10
 };
 let karo9: Spielkarte = {
     farbe: "Karo",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 11
 };
 let karo10: Spielkarte = {
     farbe: "Karo",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 12
 };
 let karoAss: Spielkarte = {
     farbe: "Karo",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 13
 };
 let karoBube: Spielkarte = {
     farbe: "Karo",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 14
 };
 let karoDame: Spielkarte = {
     farbe: "Karo",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 15
 };
 let karoKönig: Spielkarte = {
     farbe: "Karo",
-    zahl: "König"
+    zahl: "König",
+    order: 16
 };
 let pik7: Spielkarte = {
     farbe: "Pik",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 17
 };
 let pik8: Spielkarte = {
     farbe: "Pik",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 18
 };
 let pik9: Spielkarte = {
     farbe: "Pik",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 19
 };
 let pik10: Spielkarte = {
     farbe: "Pik",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 20
 };
 let pikAss: Spielkarte = {
     farbe: "Pik",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 21
 };
 let pikBube: Spielkarte = {
     farbe: "Pik",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 22
 };
 let pikDame: Spielkarte = {
     farbe: "Pik",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 23
 };
 let pikKönig: Spielkarte = {
     farbe: "Pik",
-    zahl: "König"
+    zahl: "König",
+    order: 23
 };
 let kreuz7: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Sieben"
+    zahl: "Sieben",
+    order: 24
 };
 let kreuz8: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Acht"
+    zahl: "Acht",
+    order: 25
 };
 let kreuz9: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Neun"
+    zahl: "Neun",
+    order: 26
 };
 let kreuz10: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Zehn"
+    zahl: "Zehn",
+    order: 27
 };
 let kreuzAss: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Ass"
+    zahl: "Ass",
+    order: 28
 };
 let kreuzBube: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Bube"
+    zahl: "Bube",
+    order: 29
 };
 let kreuzDame: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "Dame"
+    zahl: "Dame",
+    order: 30
 };
 let kreuzKönig: Spielkarte = {
     farbe: "Kreuz",
-    zahl: "König"
+    zahl: "König",
+    order: 31
 };
 
-document.addEventListener('DOMContentLoaded', write_htmlbody);
+document.addEventListener("DOMContentLoaded", write_htmlbody);
 
-function write_htmlbody () {
-    let body : string = `<div class="top_interface">
+function write_htmlbody(): void {
+    let body: string = `<div class="top_interface">
     <div>
         <p class="rundenzahl">Rundenzahl:</p>
     </div>
         <p class="countdown">30s/20s/10s</p>`;
-    document.getElementById("createHtml").insertAdjacentHTML('beforeend', body);
+    document.getElementById("createHtml").insertAdjacentHTML("beforeend", body);
     neues_spiel();
+    document.getElementById("ziehstapel").addEventListener("click", cards_ziehen);
+    document.getElementById("sort").addEventListener("click", cards_sort);
 }
+    
 
 let ziehstapel: Spielkarte[] = [herz7, herz8, herz9, herz10, herzAss, herzBube, herzDame, herzKönig, karo7, karo8, karo9, karo10, karoAss, karoBube, karoDame, karoKönig, pik7, pik8, pik9, pik10, pikAss, pikBube, pikDame, pikKönig, kreuz7, kreuz8, kreuz9, kreuz10, kreuzAss, kreuzBube, kreuzDame, kreuzKönig];
 let handkarten: Spielkarte[] = [];
+let ablagestapel: Spielkarte[] = [];
 
 function neues_spiel(): void {
-    let anfangskarten : number
+    let anfangskarten: number;
     ziehstapel = [herz7, herz8, herz9, herz10, herzAss, herzBube, herzDame, herzKönig, karo7, karo8, karo9, karo10, karoAss, karoBube, karoDame, karoKönig, pik7, pik8, pik9, pik10, pikAss, pikBube, pikDame, pikKönig, kreuz7, kreuz8, kreuz9, kreuz10, kreuzAss, kreuzBube, kreuzDame, kreuzKönig];
-    handkarten.length = 0
+    handkarten.length = 0;
     document.getElementById("handkarten").innerHTML = " ";
-    
-    do{
+
+    do {
         anfangskarten = parseInt(prompt("Wie viele Handkarten? Von einer bis 32"), 10);
-    } while(isNaN(anfangskarten) || anfangskarten > 32 || anfangskarten < 1)                                                                                                                                                                                                                                                                                                                                                                                                                                                              < 1)
-    
-        document.getElementById("handkartenAnzahl").innerHTML = "Anzahl Handkarten:" + " " + anfangskarten;
-        random_cards(anfangskarten);
-        console.log(anfangskarten); 
-    }
+    } while (isNaN(anfangskarten) || anfangskarten > 32 || anfangskarten < 1);
+
+    document.getElementById("handkartenAnzahl").innerHTML = "Anzahl Handkarten:" + " " + anfangskarten;
+    random_cards(anfangskarten);
+    console.log(anfangskarten);
+    document.getElementById("neuesspiel").addEventListener("click", neues_spiel);
+}
 
 
-function random_cards(_anfangskarten: number){
-    let i: number = 0; 
-    let x: number;   
-    while (i < _anfangskarten){
-        x = Math.floor((Math.random() * ziehstapel.length) + 0);  
-        console.log("Anzahl Ziehstapel:" + " " + ziehstapel.length)
+function random_cards(_anfangskarten: number): void {
+    let i: number = 0;
+    let x: number;
+    while (i < _anfangskarten) {
+        x = Math.floor((Math.random() * ziehstapel.length) + 0);
+        console.log("Anzahl Ziehstapel:" + " " + ziehstapel.length);
         console.log("Zufallsnummer:" + " " + x);
         console.log(ziehstapel[x]);
-        
+
         handkarten.push(ziehstapel[x]);
-        console.log(handkarten[i])
-        ziehstapel.splice(x,1);
-        
+        console.log(handkarten[i]);
+        ziehstapel.splice(x, 1);
+
         let kartenFarbe: string = handkarten[i].farbe;
         let kartenNummer: string = handkarten[i].zahl;
+        let kartenID: number = handkarten[i].order;
         console.log(kartenFarbe, kartenNummer);
         create_cards(kartenFarbe, kartenNummer);
-        i++
+        i++;
     }
-        create_ziehstapel(ziehstapel.length)
-    }
+    create_ziehstapel(ziehstapel.length);
+}
 
-function create_cards(_kartenFarbe: string, _kartenNummer: string){
-    switch(_kartenFarbe){
+function create_cards(_kartenFarbe: string, _kartenNummer: string): void {
+    switch (_kartenFarbe) {
         case "Herz":
         case "Karo":
-        let cardsred : string = `<div class="flex"><ul><li class="farberot">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
-        document.getElementById("handkarten").insertAdjacentHTML('beforeend', cardsred);
-        break;
+            let cardsred: string = `<div class="flex"><ul><li class="farberot">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
+            document.getElementById("handkarten").insertAdjacentHTML("beforeend", cardsred);
+            break;
         default:
-        let cardsblack : string = `<div class="flex"><ul><li class="farbeschwarz">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
-        document.getElementById("handkarten").insertAdjacentHTML('beforeend', cardsblack);
-        break;
+            let cardsblack: string = `<div class="flex"><ul><li class="farbeschwarz">${_kartenFarbe}</li><li class="zahl">${_kartenNummer}</li></ul></div>`;
+            document.getElementById("handkarten").insertAdjacentHTML("beforeend", cardsblack);
+            break;
     }
 }
 
-function create_ziehstapel(_ziehstapelanzahl: number){
-    let ziehstapel : string = `<div class="ziehstapel"><p id="innerZiehstapel"></p></div>`;
-    document.getElementById("handkarten").insertAdjacentHTML('beforeend', ziehstapel);
+function create_ziehstapel(_ziehstapelanzahl: number): void {
+    let ziehstapel: string = `<div class="ziehstapel" id="ziehstapel"><p id="innerZiehstapel"></p></div>`;
+    document.getElementById("ziehstapel").insertAdjacentHTML("beforeend", ziehstapel);
     document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + _ziehstapelanzahl;
 }
 
 
 
 function cards_ziehen(): void {
-    let i: number = 0; 
-    let x: number;   
-    while (i < 1){
-        x = Math.floor((Math.random() * ziehstapel.length) + 0); 
-        handkarten.push(ziehstapel[x]);
-        ziehstapel.splice(x,1);
-        let kartenFarbe: string = handkarten[handkarten.length-1].farbe;
-        let kartenNummer: string = handkarten[handkarten.length-1].zahl;
-        create_cards(kartenFarbe, kartenNummer);
-        document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + ziehstapel.length;
-        i++
+    let x: number;
+    x = Math.floor((Math.random() * ziehstapel.length) + 0);
+    handkarten.push(ziehstapel[x]);
+    ziehstapel.splice(x, 1);
+    let kartenFarbe: string = handkarten[handkarten.length - 1].farbe;
+    let kartenNummer: string = handkarten[handkarten.length - 1].zahl;
+    create_cards(kartenFarbe, kartenNummer);
+    document.getElementById("innerZiehstapel").innerHTML = "Ziehstapel:" + " " + ziehstapel.length;
+    document.getElementById("handkartenAnzahl").innerHTML = "Anzahl Handkarten:" + " " + handkarten.length;
     }
 
-    }
+function cards_sort(): void {
+    let i: number = 0;
+    document.getElementById("handkarten").innerHTML = " ";
+    handkarten.sort(array_sort);
+    console.log(handkarten);
+    while (i < handkarten.length) {
+    create_cards(handkarten[i].farbe, handkarten[i].zahl);
+    i++;
+}}
+
+function array_sort(_a: Spielkarte, _b: Spielkarte): number { 
+    return _a.order - _b.order;
+}
