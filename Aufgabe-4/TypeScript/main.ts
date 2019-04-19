bd4f3f1acb79a7faa63a15ebe25031c8ef6ee161
LMKodalle <LMKodalle@gmail.com>
2019-04-19 13:11:55 +0200

    index

--- Aufgabe-4/TypeScript/main.ts
+++ Aufgabe-4/TypeScript/main.ts
@@ -1,24 +1,49 @@
 window.addEventListener("load", rdy);
 
 function rdy(_event: Event): void {
+    document.getElementById("bestellen").addEventListener("click", missing);
     console.log("loaded");
     let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
     for (let i: number = 0; i < fieldsets.length; i++) {
         let fieldset: HTMLFieldSetElement = fieldsets[i];
-        fieldset.addEventListener("change", handlechange);
+        fieldset.addEventListener("change", click);
     }
 }
 
-function handlechange(_event: Event): void {
-    let target: HTMLInputElement = <HTMLInputElement>_event.target; 
-    
-    switch (target.id) {
-        case "scoop1" :
-        case "scoop2" :
-        case "scoop3" :
-        case "scoop4" :
-        case "scoop5" : document.getElementById("price").innerHTML = target.value + " " + "€"; break;
-
-    }
+function click(_event: Event): void {       
+    let x: number= 0;
+    let check: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+    document.getElementById("sorten").innerHTML = " ";
+    document.getElementById("toppings").innerHTML = " ";
+    for (let i: number = 0; i < check.length; i++) {
+        if (check[i].checked == true) {
+            let price: number = Number(check[i].value)
+            x += price;
+            document.getElementById("price").innerHTML = x.toFixed(2).toString() + " " + "€";
+            let bezeichung = document.createElement("li");
+            bezeichung.innerHTML = `${check[i].id}`
+            document.getElementById("toppings").appendChild(bezeichung)
+        }
+    if (check[i].type == "number" && Number(check[i].value) > 0) {
+            let sorten = document.createElement("li");
+            sorten.innerHTML = `${check[i].value} x ${check[i].name}`
+            document.getElementById("sorten").appendChild(sorten)
+        }
+    } 
 }
+
+function missing(): void {
+    let empty: string[] = [];
+    let missing: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+    for (let i: number = 0; i < missing.length; i++) {
+        if (missing[i].value == "") {
+            let feldName: string = missing[i].name
+            empty.push(feldName)
+        }
+    } if (empty.length == 0) {
+        alert("danke fur ihre Bestellung");
+    } else { alert(`Du musst unbedingt noch ${empty} angeben`); }
+}
+
+     
