6c6283095bac271f3fcf4dded044ea1647eaba27
Lukas Kodalle <LMKodalle@gmail.com>
2019-05-23 13:24:23 +0200

    update

--- Aufgabe-9/Server/Database.ts
+++ Aufgabe-9/Server/Database.ts
@@ -8,7 +8,7 @@ import * as Mongo from "mongodb";
 console.log("Database starting");
 
 let databaseURL: string = "mongodb://localhost:27017";
-let databaseName: string = "Test";
+let databaseName: string = "test";
 let db: Mongo.Db;
 let students: Mongo.Collection;
 
